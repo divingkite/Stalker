f682bbb928f6495397559e216bc8e83ec5fb5ab7
divingkite <hiteshagrawal6@gmail.com>
2016-02-23 23:25:28 +0530

    Added regitration url. views.py edited for adding authentication(OAuth2) feature,added registration view

--- Stalker/urls.py
+++ Stalker/urls.py
@@ -1,7 +1,7 @@
 """Stalker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+    https://docs.djangoproject.com/en/1.7/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,44 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
+admin.autodiscover()
+from stalker_api import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns(
+	'',
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    # url for generating access token
+
+    url(r'^expand_list/$',views.expand_list ),
+    # url for adding contacts.
+
+    url( r'^friend_list/$' , views.list_ , name = 'friend_list'),
+    # url for fetching list of all contacts.
+
+    url( r'^refresh/$', views.update_info,name='refresh'), 
+    # url for updating every contacts information/data.
+    
+    url(r'^profile/(?P<person_id>[0-9]+)/$', views.profile , name='profile'), 
+    # url to view questions done by a particular contact.
+    
+    url( r'^delete/(?P<person_id>[0-9]+)/$' , views.delete_contact , name = 'remove_from_list'), # remove a person in the list of friends
+    # url for deleting a contact
+
+    url( r'^search_contest/(?P<site>\w+)/$', views.search_contestwise ,name='contest_peformance' ),       #shows list of all contests.
+    # url to get list of all contest in either codechef/codeforces.
+    # for codechef  : /search_contest/Codechef/
+    # for codeforces: /search_contest/Codeforces/
+
+    url( r'^contest_performance/(?P<site>\w+)/(?P<contest>\w+)/$',views.SearchForThisContest,name='list_for_a_contest'),    #shows everyone performance in a contest
+    # url for fetching questions done by all contacts in a particular contest
+    # for codechef   : /contest_performance/Codechef/contest_name
+    # for codeforces : /contest_performance/Codeforces/contestId
+
+    url(r'^refresh/(?P<person_id>[0-9]+)/$', views.update_a_person_info , name='refresh_a_person'), #to update information of a particular contacts.
+    # url for updating a particular contact,provided by person_id/pk ,information/data
+
+    url(r'^register/$', views.Registration) ,
+    # url for registration of a new user    
+)
--- stalker_api/views.py
+++ stalker_api/views.py
@@ -1,17 +1,15 @@
-from .models import *
+from stalker_api.models import *
 from django.http import HttpResponseRedirect, HttpResponse
 from stalker_api.helper_functions import *
-from django.contrib.auth import logout
-from django.contrib.auth import login as auth_login
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from .serializer import *
+from stalker_api.serializer import *
 from rest_framework.renderers import JSONRenderer
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from stalker_api.permissions import IsOwner
+from rest_framework import permissions
+from rest_framework.views import APIView
+from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
 class JSONResponse(HttpResponse):
     """
@@ -22,174 +20,169 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-"""@csrf_exempt
-def register(request):
-    '''
-    To register a user, input is via RegistrationForm.
-    '''
-    if request.method == 'POST':
-    	data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            print serializer.data
-            user = authenticate( username = serializer.validated_data['username'], password = serializer.validated_data['password'] )
-            auth_login( request,user )
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
-     
-"""
 @csrf_exempt
-def logout_page(request):
-    if request.method == "GET":
-        logout(request)
-        return JSONResponse({"result":"logout"},status=200)
+@api_view(['POST'])
+@permission_classes(())
+def Registration(request):
+    '''
+    For registration of new users.
+    '''
+    data = JSONParser().parse(request)
+    serializer = RegistrationSerializer(data=data) 
+    if serializer.is_valid():                           #user can be created
+        data = serializer.data
+        user = User.objects.create(username=data['username'])
+        user.set_password(data['password'])
+        user.save() 
+        return JSONResponse(serializer.data, status=201)
+    return JSONResponse(serializer.errors, status=400)   # user not creatd
 
-@csrf_exempt
-def login(request):
-    '''
-    Upon login redirects to the friend_list page.
-    '''
-    if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = UserSerializer( data = data )
-        
-        if serializer.is_valid():
-            print serializer.data["username"],serializer.validated_data['username']
-            user = authenticate( username = serializer.validated_data['username'], password = serializer.validated_data['password'] )
-            print user
-            if user is not None:
-                auth_login( request,user )
-                return JSONResponse({},status=200)
-            return JSONResponse({},status=400)
 
 @api_view(['GET'])
-#@permission_classes((IsOwner, ))
-def home(request):
-    return JSONResponse({"user":"csdsd"},status=200)
-            #return JSONResponse({"fail":"csdsd"},status=200)
-
-    #else:
-     #   return HttpResponse({})
-
-#@permission_classes((IsAuthenticated, ))
+@permission_classes((permissions.IsAuthenticated, TokenHasReadWriteScope,))
 def update_info(request):
     '''
-    For the refresh button, which updates all information about all contacts.
+    For the refresh url, which updates all information about all contacts.
     '''
-    if request.method == "GET":
-        current = HelperFunctions(request)
-        current.FetchContent()
-        objs = Person.objects.filter()
-        serializer = PersonSerializer( objs, many=True)
-        return JSONResponse(serializer.data,status=200)
-
-#@permission_classes((IsAuthenticated, ))
-def list_( request ):
-    #if request.user.is_authenticated():
-    if request.method == "GET":
-        objs = Person.objects.filter()
-        serializer = PersonSerializer(objs,many = True )
-        return JSONResponse(serializer.data,status=200)
-
-#@permission_classes((IsAuthenticated, ))
-@csrf_exempt
-def expand_list( request ):
-    if request.method == "POST":
-    	data = JSONParser().parse(request)
-        serializer = PersonSerializer(data = data)    
-        if serializer.is_valid():
-        	serializer.save()
-        	return JSONResponse(serializer.data,status=200)
-        return JSONResponse(serializer.errors,status=400)
-
-#@permission_classes((IsAuthenticated, ))
-def delete_contact( request,person_id ):
-	#if request.method == "DELETE":
-	p_object = Person.objects.get( pk = person_id )
-	if p_object is not None:
-		p_object.delete()
-		return HttpResponse(status=204)
-	else:
-		return HttpResponse(status=404)
-        
-
-#@permission_classes((IsAuthenticated, ))
-def profile( request,person_id ):
-    if request.method == "GET":
-        person = Person.objects.get(pk=person_id)
-        cf_contest = Contest.objects.filter( site ='CF' )
-        cf_obj=[]                                        # a list containing [[contest name,[questions]],[]] 
-        
-        for contest in cf_contest:                        # fetching all contest problems
-            ques = Question.objects.filter( contest = contest,person=person )
-            if len(ques) != 0:
-                serializer = QuestionSerializer(ques,many=True)
-            	cf_obj.append( [contest.name,serializer.data ] )
-        
-        cc_contest = Contest.objects.filter( site='CC' )
-        
-        cc_obj=[]
-        for contest in cc_contest:
-            ques = Question.objects.filter( contest = contest, person = person )
-            if len(ques) != 0:
-            	serializer = QuestionSerializer(ques,many=True)
-                cc_obj.append( [contest.name,serializer.data ] )
-            
-        #cf_prac = PracticeProb.objects.filter( person = person,site='CF' )     #fetching all practice problems.
-        #cc_prac = PracticeProb.objects.filter( person = person,site='CC' )
-        
-        return JSONResponse( { 'cf_data_chal' : cf_obj ,
-                               'cc_data_chal' : cc_obj ,
-                               #'cc_data_prac' : cc_prac ,
-                               #'cf_data_prac' : cf_prac 
-                               } )
-
-#@permission_classes((IsAuthenticated, ))
-def search_contestwise( request,site ):
-    '''
-    Gives list of all contest when site ( CC,CF ) is provided
-    '''
-    #if request.user.is_authenticated():
-    if request.method == "GET":
-        cons = Contest.objects.filter( site=site )
-        serializer = ContestSerializer(cons,many=True)
-        return JSONResponse(serializer.data,status=200)
-    #else :
-     #   return HttpResponseRedirect( reverse( 'stalkerNew:login' ))
-
-#@permission_classes((IsAuthenticated, ))
-def SearchForThisContest(request,site,contest):
-    '''
-    Fetches all the questions of all contacts when a contest in selected in contest.html page.
-    '''
-    if request.method == "GET" :
-        questions_done_by_all_person = []
-        if site == "CF":
-            contest = Contest.objects.get(contestId = contest)
-        else:
-            contest = Contest.objects.get(name=contest)
-        #persons = Person.objects.filter( user = request.user )
-        persons = Person.objects.filter()
-        for person in persons:
-            ques = Question.objects.filter( person = person, contest = contest )
-            qserializer = QuestionSerializer(ques,many=True)
-            pserializer = PersonSerializer(person)
-            questions_done_by_all_person.append( [pserializer.data,qserializer.data] )
-        
-        return JSONResponse( questions_done_by_all_person ,status=200)
-
-#@permission_classes((IsAuthenticated, ))
-@csrf_exempt
-def update_a_person_info(request,person_id):
-    '''
-    Updates information about a particular contact.
-    '''
-    if request.method == "GET":
-        current = HelperFunctions(request,person_id)
-        current.FetchContentForAPerson()
-        objs = Person.objects.filter()
-        serializer = PersonSerializer(objs,many-True)
+    current = HelperFunctions(request=request)
+    current.FetchContent()
+    objs = Person.objects.filter(owner=request.user)
+    serializer = PersonSerializer( objs, many=True)
     return JSONResponse(serializer.data,status=200)
 
 
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+def list_( request ):
+    '''
+    For url friend_list, provides a list of all contacts.
+    '''
+    objs = Person.objects.filter(owner=request.user)
+    for o in objs:
+        print o.name
+    serializer = PersonSerializer(objs,many = True )
+    data = { 'persons':serializer.data }
+    return JSONResponse(data,status=200)
+
+
+@api_view( ['POST'] )
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+@csrf_exempt
+def expand_list( request ):
+    '''
+    For url expand_list, used to add new contacts.
+    '''
+    data = JSONParser().parse(request)
+    serializer = PersonSerializer(data=data)
+    if serializer.is_valid():
+        data = serializer.data
+        cc_status = check_codechef_handle(data['codechef_handle'])      
+        #True if correct handle name is provided in request, else false.
+
+        cf_status = check_codeforces_handle(data['codeforces_handle'])
+        #True if correct handle name is provided in request, else false.
+
+        if cc_status and cf_status:
+            #if both handle names are correct
+            p = Person(name=data['name'],codechef_handle=data['codechef_handle'],
+                                         codeforces_handle=data['codeforces_handle']
+                                         ,owner=request.user ) 
+            p.save()
+            data = { 'result':serializer.data }
+            return HttpResponse( status=200 )
+        else:
+            HttpResponse(400)         #not cottect details provided for handles
+    return JSONResponse(serializer.errors,status=400)   #Bad request
+    
+
+@api_view( ['DELETE'] )
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+def delete_contact( request,person_id ):
+    '''
+    Deletes a contact provided its person_id/pk from Person class.
+    '''
+    p_object = Person.objects.get( pk = person_id )
+    if p_object is not None:
+        p_object.delete()
+        return HttpResponse(status=204)    
+    else :
+        return HttpResponse(status=400)  #Bad request
+
+@api_view( ['GET'] )
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+def profile( request,person_id ):
+    '''
+    Gives all questions done a particular contact provided its person_id/pk from Person class.
+    '''
+    person = Person.objects.get(owner=request.user,pk=person_id)
+    
+    cf_contest = Contest.objects.filter( site ='Codeforces' )
+    cf_obj=[]                                         
+    for contest in cf_contest:                        # fetching all contest problems
+        ques = Question.objects.filter( contest = contest,person=person )
+        if len(ques) != 0:
+            serializer = QuestionSerializer(ques,many=True)
+            cf_obj.append( [ {'Contest name':contest.name},{'questions':serializer.data} ] )
+        
+    cc_contest = Contest.objects.filter( site='Codechef' )
+    cc_obj=[]
+    for contest in cc_contest:
+        ques = Question.objects.filter( contest = contest, person = person )
+        if len(ques) != 0:
+            serializer = QuestionSerializer(ques,many=True)
+            cc_obj.append( [ {'Contest name':contest.name},{'questions':serializer.data} ] )
+            
+             
+    return JSONResponse( { 'codeforces_problems' : cf_obj ,
+                            'codechef_problems' : cc_obj ,
+                         } )
+
+@api_view( ['GET'] )
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+def search_contestwise( request,site ):
+    '''
+    Gives list of all contest when site ( Codechef/Codeforces ) is provided
+    '''
+    print request.user
+    cons = Contest.objects.filter( site=site )
+    serializer = ContestSerializer(cons,many=True)
+    data = {'contests':serializer.data}
+    return JSONResponse(data,status=200)
+    
+@api_view( ['GET'] )
+@permission_classes((permissions.IsAuthenticated,TokenHasReadWriteScope, ))
+def SearchForThisContest(request,site,contest):
+    '''
+    for codechef   : write contest name.
+    for codeforces : write contestId.
+    Fetches all the questions of all contacts when a contest in selected
+    '''
+    print request.user
+    questions_done_by_all_person = []
+    if site == "Codeforces":
+        contest = Contest.objects.get(contestId = contest)
+    else:
+        contest = Contest.objects.get(name=contest)
+    persons = Person.objects.filter( owner = request.user )
+    #persons = Person.objects.filter()
+    for person in persons:
+        ques = Question.objects.filter( person = person, contest = contest )
+        qserializer = QuestionSerializer(ques,many=True)
+        pserializer = PersonSerializer(person)
+        questions_done_by_all_person.append( [pserializer.data,qserializer.data] )
+        
+    return JSONResponse( questions_done_by_all_person ,status=200)
+
+@api_view( ['GET'] )
+@permission_classes((permissions.IsAuthenticated, TokenHasReadWriteScope,))
+@csrf_exempt
+def update_a_person_info(request,person_id):
+    '''
+    Updates information about a particular contact provided its person_id/pk from Person class.
+    '''
+    print request.user
+    current = HelperFunctions(request,person_id)
+    current.FetchContentForAPerson()
+    objs = Person.objects.filter()
+    serializer = PersonSerializer(objs,many-True)
+    return JSONResponse(serializer.data,status=200)
